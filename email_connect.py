287c445c028730a5770eb125e1b44def7261b9b4
schmidb22 <schmidb@mcmaster.ca>
2021-03-06 21:45:33 -0500

    updated file to attach pictures to email

--- email_connect.py
+++ email_connect.py
@@ -1,31 +1,67 @@
-import smtplib, ssl
+def send_email(filenames):
+    
+    import smtplib, ssl, email
+    from email import encoders
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.image import MIMEImage
+    import yaml
+#Gets config info so we can access the user email address
+##    try:
+##        config = yaml.safe_load(open("config.yml", "r"))
+##    except FileNotFoundError as e:
+##        logger.info(
+##            "Error: No config.yml found, using placeholder values, which will not work!"
+##    )
 
-sender = "camera.notifier99@gmail.com"
-recipient = "mattmione37@gmail.com"
+    sender = "camera.notifier99@gmail.com"
+    recipient = "brooklyn.schmidt22@gmail.com"
+    #recipient = config.get('email')
+    subject = "NOTIFICATION FROM SECURITY SERVICE"
+    body = 'Dear User, \n\nThis message is to notify you that your '+ \
+           'security system has detected movement in front ' +\
+           'of your camera. Please find attached photos of the incident ' +\
+           'that was detected. \nPlease do not reply to this message as '+\
+           'this email is not monitored.\n\nThank you, \nTeam Little Brother '
+    password = str(input("Type your password: "))
 
-subject = "NOTIFICATION FROM SECURITY SERVICE"
-body = "Hello! " # This will have to change based on the context when we send.
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    
+    message.attach(MIMEText(body,"plain"))#attach body to email
 
-# Assemble the subject + body using Python's string interpolation.
-message = f'Subject: {subject}\n\n{body}'
-password = str(input("Type your password: "))
+    if type(filenames) == str:#if there is only one file
+        fp = open(filenames,'rb')
+        image = MIMEImage(fp.read())
+        fp.close()
+        image.add_header('Content-ID','<image1>')#attach image to email
+        message.attach(image)
+    elif type(filenames) == list:#if there are multiple files
+        for i in range (0,len(filenames)):
+            fp = open(filenames[i],'rb')
+            image = MIMEImage(fp.read())
+            fp.close()
+            image.add_header('Content-ID','<image1>')#attach each image to email
+            message.attach(image)
+    
 
-# Creating a server object from the smtplib library on port 587. 
-# Port 587 is the TCP port that most modern email clients use.
+    text = message.as_string()
 
-# Server uses SMTP protocol.
-server = smtplib.SMTP('smtp.gmail.com',587);
+    # Creating a server object from the smtplib library on port 587. 
+    # Port 587 is the TCP port that most modern email clients use.
 
-# Using TLS encryption to send the emails, uing the starttls() method.
-# First we need to check if the TLS encryption has happened using ehlo()
-# However this is done implicitly by startls() if the server requires it. 
-server.ehlo()
-server.starttls();
-server.ehlo()
+    # Server uses SMTP protocol.
 
-server.login(sender,password);
-
-print("Login was a success. ");
-server.sendmail(sender, recipient, message);
-
-print("Email sent to email specified.");
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        try:#if login attempt fails then an exception is thrown
+            server.login(sender, password)
+        except:
+            print("Incorrect password, email failed to send.")
+            return
+        print("Login was a success. ");
+        server.sendmail(sender, recipient, text)
+        print("Email sent to email specified.");    
