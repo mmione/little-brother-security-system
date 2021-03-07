faed15352df9e727df1cb7a271c9d862411fa686
rmione <rmione33@gmail.com>
2021-03-07 10:13:24 -0500

    Change threshold bound and conditional, README update, password fix, refactoring

--- email_connect.py
+++ email_connect.py
@@ -6,61 +6,62 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 import yaml
 
+
 def send_email(filenames, password):
-#Gets config info so we can access the user email address
+    # Gets config info so we can access the user email address
+    config = {}
     try:
         config = yaml.safe_load(open("config.yml", "r"))
     except FileNotFoundError as e:
-        logger.info(
+        print(
             "Error: No config.yml found, using placeholder values, which will not work!"
-    )
-
-    sender = config.get('sender')
-    recipient = config.get('email')
+        )
+    sender = config.get("sender")
+    recipient = config.get("email")
     subject = "NOTIFICATION FROM SECURITY SERVICE"
-    body = 'Dear User, \n\nThis message is to notify you that your '+ \
-           'security system has detected movement in front ' +\
-           'of your camera. Please find attached photos of the incident ' +\
-           'that was detected. \nPlease do not reply to this message as '+\
-           'this email is not monitored.\n\nThank you, \nTeam Little Brother '
-    #password = config.get('password')
+    body = (
+        "Dear User, \n\nThis message is to notify you that your "
+        + "security system has detected movement in front "
+        + "of your camera. Please find attached photos of the incident "
+        + "that was detected. \nPlease do not reply to this message as "
+        + "this email is not monitored.\n\nThank you, \nTeam Little Brother "
+    )
 
     message = MIMEMultipart()
     message["From"] = sender
     message["To"] = recipient
     message["Subject"] = subject
-    
-    message.attach(MIMEText(body,"plain"))#attach body to email
 
-    if type(filenames) == str:#if there is only one file
-        fp = open(filenames,'rb')
+    message.attach(MIMEText(body, "plain"))  # attach body to email
+
+    if type(filenames) == str:  # if there is only one file
+        fp = open(filenames, "rb")
         image = MIMEImage(fp.read())
         fp.close()
-        image.add_header('Content-ID','<image1>')#attach image to email
+        image.add_header("Content-ID", "<image1>")  # attach image to email
         message.attach(image)
-    elif type(filenames) == list:#if there are multiple files
-        for i in range (0,len(filenames)):
-            fp = open(filenames[i],'rb')
+    elif type(filenames) == list:  # if there are multiple files
+        for i in range(0, len(filenames)):
+            fp = open(filenames[i], "rb")
             image = MIMEImage(fp.read())
             fp.close()
-            image.add_header('Content-ID','<image1>')#attach each image to email
+            image.add_header("Content-ID", "<image1>")  # attach each image to email
             message.attach(image)
-    
 
     text = message.as_string()
 
-    # Creating a server object from the smtplib library on port 587. 
+    # Creating a server object from the smtplib library on port 587.
     # Port 587 is the TCP port that most modern email clients use.
 
     # Server uses SMTP protocol.
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        try:#if login attempt fails then an exception is thrown
+        try:  # if login attempt fails then an exception is thrown
             server.login(sender, password)
         except:
             print("Incorrect password, email failed to send.")
             return
-        print("Login was a success. ");
+        print("Login was a success. ")
         server.sendmail(sender, recipient, text)
-        print("Email sent to email specified.");    
+        print("Email sent to email specified.")
